577821b97145730f80d3876c9b61ffe19fa649ac
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-03-27 21:21:45 -0300

    feat:add config admin

--- Cadastro/cadastro/admin.py
+++ Cadastro/cadastro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from cadastro.models import Cadastro
 # Register your models here.
+@admin.register(Cadastro)
+class CadastroAdmin(admin.ModelAdmin):
+    pass
--- Cadastro/cadastro/models.py
+++ Cadastro/cadastro/models.py
@@ -7,3 +7,5 @@ class Cadastro (models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
     data = models.DateTimeField(auto_now_add= True)
+    def __str__(self):
+        return f'{self.name} [{self.email}]'
